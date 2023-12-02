5268fb9275364c46ee032320b494cb9c7de87c40
Paul Griffith <pgriffith@ia.io>
2023-12-02 13:56:27 -0800

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val first = line.first { it.isDigit() }
+            val last = line.last { it.isDigit() }
+            "$first$last".toInt()
+        }
     }
+
+    val digits = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+    ) + (1..9).map(Int::toString)
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val firstS = line.findAnyOf(digits)!!.second
+            val first = digits.indexOf(firstS).mod(9) + 1
+            val lastS = line.findLastAnyOf(digits)!!.second
+            val last = digits.indexOf(lastS).mod(9) + 1
+
+            "$first$last".toInt()
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(readInput("D01T01")) == 142)
+    check(part2(readInput("D01T02")) == 281)
 
-    val input = readInput("Day01")
+    val input = readInput("D01")
     part1(input).println()
     part2(input).println()
 }
